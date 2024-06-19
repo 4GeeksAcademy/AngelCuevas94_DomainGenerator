96b0b4fe1db92eb8a9eaef0749181332484fede3
AngelCS94 <163451592+AngelCS94@users.noreply.github.com>
2024-06-19 19:27:11 +0000

    terminadisimo

--- src/domaingenerator.js
+++ src/domaingenerator.js
@@ -7,7 +7,7 @@ for (let i = 0; i < pronoun.length; i++) {
   for (let j = 0; j < adj.length; j++) {
     for (let k = 0; k < noun.length; k++) {
       for (let l = 0; l < domain.length; l++) {
-        console.log(pronoun[i], adj[j], noun[k], domain[l]);
+        console.log(pronoun[i] + adj[j] + noun[k] + domain[l]);
       }
     }
   }
